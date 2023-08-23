3688c05f599dcfa95bf22c60ca4c4203df2ef936
Currie-NGUYEN <currie@enclave.vn>
2023-08-23 14:19:57 +0700

    init project

---
+++ Instagram-Clone/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram-Clone
+//
+//  Created by Cody on 8/23/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
